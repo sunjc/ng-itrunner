0707d6e04365ed5c1a218523e6d6853f498e9d51
Sunjc <sjc-925@163.com>
2020-04-09 22:05:07 +0800

    add schematics

---
+++ components/schematics/ng-add/index.ts
@@ -0,0 +1,9 @@
+import {Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
+import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks';
+
+export default function(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    return tree;
+  };
+}
